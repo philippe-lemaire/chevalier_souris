dede3241a243a8b44a83086365796792414b298f
Philippe Lemaire <philippe.lemaire@protonmail.com>
2022-05-17 10:47:12 +0200

    adventure site v1 ok

--- toolbox/urls.py
+++ toolbox/urls.py
@@ -7,4 +7,7 @@ app_name = "toolbox"
 urlpatterns = [
     path("", views.index, name="index"),
     path("colony/", views.generate_colony, name="generate_colony"),
+    path(
+        "adventure_site/", views.generate_adventure_site, name="generate_adventure_site"
+    ),
 ]
