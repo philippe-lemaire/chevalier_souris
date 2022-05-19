80035b087b264d4eca19b7b3d683cb4f74b97900
Philippe Lemaire <philippe.lemaire@protonmail.com>
2022-05-19 09:09:46 +0200

    added variation random highlighting

--- creatures/views.py
+++ creatures/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
@@ -14,3 +16,10 @@ class IndexView(generic.ListView):
 
 class CreatureDetailView(generic.DetailView):
     model = Creature
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context["roll"] = random.randint(1, 6)
+        return context
